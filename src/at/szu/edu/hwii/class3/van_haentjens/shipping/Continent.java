ffe8ea04eb79daab55002d7c538f2ff6e631369a
Erik van Haentjens <erik.van_haentjens@edu.szu.at>
2025-12-03 10:50:59 +0100

    Aufgabe 1

---
+++ src/at/szu/edu/hwii/class3/van_haentjens/shipping/Continent.java
@@ -0,0 +1,5 @@
+package at.szu.edu.hwii.class3.van_haentjens.shipping;
+
+public enum Continent {
+    Afrika,Asien,Amerika,Australien,Europa
+}
---
+++ src/at/szu/edu/hwii/class3/van_haentjens/shipping/GermanArticle.java
@@ -0,0 +1,5 @@
+package at.szu.edu.hwii.class3.van_haentjens.shipping;
+
+public enum GermanArticle {
+    Der,Die,Das
+}
